723b1626c0f9029f456842e6f739721d3cb103c2
almanshow <Alricou@MacBook-Pro-de-Alric-3.local>
2014-11-17 11:48:56 +0100

    new

---
+++ index.php
@@ -0,0 +1,26 @@
+<div class="jumbotron">
+  <div class="container">
+  <h1>Just Get It </h1>
+  <p>A new way to buy...</p>
+  
+  </div></div>
+  
+  <div class="how">
+  <div class="container">
+    <div> <h3>Create una cuenta</h3>
+          <p>Create una cuenta de forma<strong> facil</strong> y <strong>seguro</strong></p>
+          <p><a href="">Learn More</a></p>
+    </div>
+    <div> <h3>Selecciona el producto</h3>
+          <p>Escoge entre una amplia <strong>variedad</strong> de productos.</p>
+          <p><a href="">Learn More</a></p>
+    </div>
+    <div> <h3>Haz tu puja!</h3>
+          <p>Podrás comprar un producto con dinero, o ofreciendo un producto a cambio.</p>
+          <p><a href="">Learn More</a></p>
+    
+
+  </div></div>
+
+</body>
+</html>
